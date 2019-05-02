d1435c051633a06ecfad7103ca630c7a07569956
sbohmann <mail@sebastian-bohmann.com>
2019-05-02 17:47:24 +0200

    Improves file system compatibility by escaping non-ascii code points

--- filenames.py
+++ filenames.py
@@ -4,7 +4,7 @@ def clean_filename(name):
         if 'A' <= char <= 'Z' or 'a' <= char <= 'z' or '0' <= char <= '9':
             result.append(char)
         elif ord(char) > 128:
-            result.append('#%x_;' % ord(char))
+            result.append('#%x_' % ord(char))
         else:
             result.append('_')
     return ''.join(result)
