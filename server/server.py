8b415654976e7fd6feba8e918dfc6ae7aad4e709
Sebastian Bohmann <mail@sebastian-bohmann.com>
2019-10-27 10:59:24 +0100

    Added server

--- server/server.py
+++ server/server.py
@@ -1,7 +1,11 @@
 from flask import Flask
+import os
+
 import podcast_list
 
 app = Flask('Podcasts')
+server_directory = os.path.dirname(os.path.abspath(__file__))
+template_directory = os.path.join(server_directory, 'templates')
 
 
 @app.route('/')
@@ -9,4 +13,4 @@ def index():
     return podcast_list.render()
 
 
-app.run(debug=True, host='0.0.0.0', port='9100')
+app.run(debug=True, host='0.0.0.0', port='9100', template_folder=template_directory)
