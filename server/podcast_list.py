cdc03c977f61ab670d0a330dfe3120c0e7bf6cc3
Sebastian Bohmann <mail@sebastian-bohmann.com>
2019-10-27 11:06:10 +0100

    Added server

--- server/podcast_list.py
+++ server/podcast_list.py
@@ -10,7 +10,15 @@ def render():
 def _podcast_names():
     result = []
     for file in os.listdir('.'):
-        if file != 'PodcastDownload' and os.path.isdir(file):
+        if _relevant_filename(file):
             result.append(file)
     result.sort()
     return result
+
+
+def _relevant_filename(file):
+    return os.path.isdir(file) and not _excluded_filename(file)
+
+
+def _excluded_filename(file):
+    return file != 'PodcastDownload' and not file.startswith('.')
